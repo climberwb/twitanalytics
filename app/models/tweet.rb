4b4ea9be91047797a29013029e3b8ec8dfa21b43
Warren <warren.kushner@gmail.com>
2015-02-02 21:40:53 -0500

    created make_tweet function

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,64 +3,23 @@ require 'dotenv'
 require 'json'
 require 'pry'
 Dotenv.load
-# @client ||= Twitter::REST::Client.new do |config|
-#       config.consumer_key        = ENV.fetch('twitter_consumer_key')
-#       config.consumer_secret     = ENV.fetch('twitter_secret_key')
-#     end
-# @tweets = @client.search("#analytic", result_type: "recent", lang: "en").map do |tweet|
-#       { author: tweet.user.screen_name, text: tweet.text }
-#     end
-# @tweets.each do |tweet|
-#   @pulled_tweet = Tweet.new()
-#   @pulled_tweet.update_attributes(:author => tweet[:author].to_s, :title=>tweet[:title].to_s)
-#   @pulled_tweet.save
-# end
-binding.pry
+
 
 class Tweet < ActiveRecord::Base
-
-  module TweetDigger
-  extend self
-
-  def process
-    post_to_rails records_from_twitter
+def self.make_tweet
+  @client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV.fetch('twitter_consumer_key')
+        config.consumer_secret     = ENV.fetch('twitter_secret_key')
+      end
+  @tweets = @client.search("#analytic", result_type: "recent", lang: "en").map do |tweet|
+        { author: tweet.user.screen_name, text: tweet.text }
+      end
+  @tweets.each do |tweet|
+    #@pulled_tweet = Tweet.new()
+    @pulled_tweet = Tweet.create!(:author => tweet[:author].to_s, :title=>tweet[:text].to_s)
+    @pulled_tweet.save
   end
-
-private
-
-  def post_to_rails(records)
-    records.each do |tweet|
-      `curl --data #{to_params(tweet)} #{rails_url}`
-    end
-  end
-
-  def records_from_twitter
-    @tweets = @client.search("#analytic", result_type: "recent", lang: "en").map do |tweet|
-      { author: tweet.user.screen_name, text: tweet.text }
-    end
-  end
-
-  def to_params(tweet)
-    encoded = [tweet[:text], tweet[:author]].map do |to_encode|
-      URI.escape(to_encode)
-    end
-    "\"tweet[text]=#{encoded[0]}&tweet[author]=#{encoded[1]}\""
-  end
-
-
-  def rails_url
-    "http://#{ENV.fetch("host")}/tweets.json"
-  end
-
-  def client
-    @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV.fetch('twitter_consumer_key')
-      config.consumer_secret     = ENV.fetch('twitter_secret_key')
-    end
-  end
-
 end
+Tweet.make_tweet
 #binding.pry
-#TweetDigger.process
-
 end
